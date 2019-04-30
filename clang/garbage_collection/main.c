073b719573742d04daf53cc1441a5d12cb026a41
wasanx25 <wasanx25@gmail.com>
2019-04-30 18:17:44 +0900

    Add malloc and free

---
+++ clang/garbage_collection/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char *concat(const char *s1, const char *s2)
+{
+  size_t l1 = strlen(s1);
+  size_t l2 = strlen(s1);
+
+  char *result = malloc(l1 + l2 + 1);
+
+  memcpy(result, s1, l1);
+  memcpy(result + l1, s2, l2);
+
+  result[l1 + l2] = '\0';
+
+  return result;
+}
+
+int main(int argc, const char *argv[])
+{
+  const char *abc = "abc";
+  const char *def = "def";
+
+  const char *abcdef = concat(abc, def);
+
+  puts(abcdef);
+
+  free((void *)abcdef);
+
+  return 0;
+}
